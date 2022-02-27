a3bb6c98039aea3b0cfc000fcf11cee5a307f844
Luigi Remor <luigiremor@github.com>
2022-02-27 01:29:28 -0300

    Countdown V0.1

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Countdown from '../components/Countdown'
 
 const Home = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative flex h-screen justify-center">
       <Head>
         <title>Luigi's Birthday Countdown</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-full w-full items-center">
+      <div className="flex h-full w-full max-w-7xl">
         <Countdown />
       </div>
     </div>
